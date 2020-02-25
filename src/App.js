f38353ebd911bdacc51075f79ebc660c96ed98f3
James Nuanez <james@jamesnuanez.com>
2020-02-24 22:08:17 -0800

    Initial commit: Cleaned up CRA + Ant Design

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Real-Time Transcription Web App</h1>
+    </div>
+  );
+}
+
+export default App;
